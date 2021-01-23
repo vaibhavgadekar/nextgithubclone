c54354a32c04d83b25e9dc358bcbab66b9689d4b
Vaibhav Gadekar <39763708+vaibhavgadekar@users.noreply.github.com>
2021-01-23 19:54:37 +0530

    Update [postNo].js

--- pages/[postNo].js
+++ pages/[postNo].js
@@ -1,1 +1,17 @@
-dkhfjg
+import { useRouter } from 'next/router'
+
+
+const Post = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  return (
+    <>
+     
+   
+    <h1>{id}</h1>
+    </>
+  )
+}
+
+export default Post
