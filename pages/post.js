9b61f159fa3432a141397f7ad0a43102c8a18890
Vaibhav Gadekar <39763708+vaibhavgadekar@users.noreply.github.com>
2021-01-23 20:59:26 +0530

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -27,7 +27,7 @@ function post({ posts }) {
   // This function gets called at build time on server-side.
   // It won't be called on client-side, so you can even do
   // direct database queries. See the "Technical details" section.
-  export async function getStaticProps() {
+  export async function getServerSideProps() {
     // Call an external API endpoint to get posts.
     // You can use any data fetching library
     const res = await fetch('https://jsonplaceholder.typicode.com/photos')
