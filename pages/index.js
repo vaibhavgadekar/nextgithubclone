f8779e7b60b3a80f2b0ee130befb558ca08c4390
Vaibhav Gadekar <39763708+vaibhavgadekar@users.noreply.github.com>
2021-01-23 19:27:54 +0530

    asd

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-     <Link href="/post" as="/post/vaibhav">
+     <Link href="/post" as="">
     <a>this page!</a>
   </Link>
 
